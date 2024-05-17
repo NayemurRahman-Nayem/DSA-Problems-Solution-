aa755950287d34e0839c4f9ae7ff3242e064b2a4
Nayemur Rahman Nayem <48326626+NayemurRahman-Nayem@users.noreply.github.com>
2024-05-18 00:05:47 +0600

    Create Post-order Traversal of Binary Tree.cpp

---
+++ Binary Trees/Post-order Traversal of Binary Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void dfs(TreeNode* root,vector<int>&ans) {
+        if(root==NULL) {
+            return ; 
+        }
+        if(root->left==NULL and root->right==NULL) {
+            ans.push_back(root->val) ; 
+            return ; 
+        }
+        if(root->left!=NULL) {
+            dfs(root->left,ans) ; 
+        }
+        if(root->right!=NULL) {
+            dfs(root->right,ans) ; 
+        }
+        ans.push_back(root->val) ; 
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans ; 
+        dfs(root,ans) ;
+        return ans ; 
+    }
+};
